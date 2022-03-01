8a2a508cea3171c6f238e0cec98363c2d9d6392e
rooiss <louis.kim010@gmail.com>
2022-02-28 16:59:08 -0800

    removed react-ga

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import { Main } from './components/Main'
-import ReactGA from 'react-ga'
-import { useEffect } from 'react'
 
 function App() {
-  useEffect(() => {
-    ReactGA.initialize('G-31LB3GQK3Y')
-    ReactGA.pageview(window.location.pathname + window.location.search)
-  }, [])
   return <Main />
 }
 
